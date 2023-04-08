22305831e76d5498ded7fb013bb8ebf9c84cde4c
BuiNguyenThienAn <90361880+ThiEnAndeptrai@users.noreply.github.com>
2023-04-08 12:49:46 +0700

    Final

---
+++ webBanHang/food.js
@@ -0,0 +1,561 @@
+import { foodItem } from './fooditem.js'
+
+
+function displayItems() {
+    var Com = document.getElementById('Com');
+    var Canh = document.getElementById('Canh');;
+    var TrangMieng = document.getElementById('TrangMieng');
+    var DoAnThem = document.getElementById('DoAnThem');
+    var DoAnKhac = document.getElementById('DoAnKhac');
+    var Nuoc = document.getElementById('Nuoc');
+
+    const ComData = foodItem.filter((item) => item.category == 'Com');
+    const TrangMiengData = foodItem.filter((item) => item.category == 'TrangMieng');
+    const CanhData = foodItem.filter((item) => item.category == 'Canh');
+    const DoAnThemData = foodItem.filter((item) => item.category == 'DoAnThem');
+    const DoAnKhacData = foodItem.filter((item) => item.category == 'DoAnKhac');
+    const NuocData = foodItem.filter((item) => item.category == 'Nuoc');
+
+    ComData.map(item => {
+
+        var itemCard = document.createElement('div');
+        itemCard.setAttribute('id', 'item-card')
+
+        var cardTop = document.createElement('div');
+        cardTop.setAttribute('id', 'card-top');
+
+        var star = document.createElement('i');
+        star.setAttribute('class', 'fa fa-star');
+        star.setAttribute('id', 'rating');
+        star.innerText = ' ' + item.rating;
+
+        var heart = document.createElement('i');
+        heart.setAttribute('class', 'fa fa-heart-o add-to-cart');
+        heart.setAttribute('id', item.id)
+
+        cardTop.appendChild(star);
+        cardTop.appendChild(heart);
+
+
+        var img = document.createElement('img');
+        img.src = item.img;
+
+        var itemName = document.createElement('p');
+        itemName.setAttribute('id', 'item-name');
+        itemName.innerText = item.name;
+
+        var itemPrice = document.createElement('p');
+        itemPrice.setAttribute('id', 'item-price');
+        itemPrice.innerText = 'Price : $ ' + item.price;
+
+        itemCard.appendChild(cardTop);
+        itemCard.appendChild(img);
+        itemCard.appendChild(itemName);
+        itemCard.appendChild(itemPrice);
+
+        Com.appendChild(itemCard);
+
+    })
+
+    TrangMiengData.map(item => {
+        var itemCard = document.createElement('div');
+        itemCard.setAttribute('id', 'item-card')
+
+        var cardTop = document.createElement('div');
+        cardTop.setAttribute('id', 'card-top');
+
+        var star = document.createElement('i');
+        star.setAttribute('class', 'fa fa-star');
+        star.setAttribute('id', 'rating');
+        star.innerText = ' ' + item.rating;
+
+        var heart = document.createElement('i');
+        heart.setAttribute('class', 'fa fa-heart-o add-to-cart');
+        heart.setAttribute('id', item.id)
+
+        cardTop.appendChild(star);
+        cardTop.appendChild(heart);
+
+
+        var img = document.createElement('img');
+        img.src = item.img;
+
+        var itemName = document.createElement('p');
+        itemName.setAttribute('id', 'item-name');
+        itemName.innerText = item.name;
+
+        var itemPrice = document.createElement('p');
+        itemPrice.setAttribute('id', 'item-price');
+        itemPrice.innerText = 'Price : $ ' + item.price;
+
+        itemCard.appendChild(cardTop);
+        itemCard.appendChild(img);
+        itemCard.appendChild(itemName);
+        itemCard.appendChild(itemPrice);
+
+        TrangMieng.appendChild(itemCard)
+
+    })
+
+    CanhData.map(item => {
+        var itemCard = document.createElement('div');
+        itemCard.setAttribute('id', 'item-card')
+
+        var cardTop = document.createElement('div');
+        cardTop.setAttribute('id', 'card-top');
+
+        var star = document.createElement('i');
+        star.setAttribute('class', 'fa fa-star');
+        star.setAttribute('id', 'rating');
+        star.innerText = ' ' + item.rating;
+
+        var heart = document.createElement('i');
+        heart.setAttribute('class', 'fa fa-heart-o add-to-cart');
+        heart.setAttribute('id', item.id)
+
+        cardTop.appendChild(star);
+        cardTop.appendChild(heart);
+
+
+        var img = document.createElement('img');
+        img.src = item.img;
+
+        var itemName = document.createElement('p');
+        itemName.setAttribute('id', 'item-name');
+        itemName.innerText = item.name;
+
+        var itemPrice = document.createElement('p');
+        itemPrice.setAttribute('id', 'item-price');
+        itemPrice.innerText = 'Price : $ ' + item.price;
+
+        itemCard.appendChild(cardTop);
+        itemCard.appendChild(img);
+        itemCard.appendChild(itemName);
+        itemCard.appendChild(itemPrice);
+
+        Canh.appendChild(itemCard)
+
+    })
+
+    DoAnThemData.map(item => {
+        var itemCard = document.createElement('div');
+        itemCard.setAttribute('id', 'item-card')
+
+        var cardTop = document.createElement('div');
+        cardTop.setAttribute('id', 'card-top');
+
+        var star = document.createElement('i');
+        star.setAttribute('class', 'fa fa-star');
+        star.setAttribute('id', 'rating');
+        star.innerText = ' ' + item.rating;
+
+        var heart = document.createElement('i');
+        heart.setAttribute('class', 'fa fa-heart-o add-to-cart');
+        heart.setAttribute('id', item.id)
+
+        cardTop.appendChild(star);
+        cardTop.appendChild(heart);
+
+
+        var img = document.createElement('img');
+        img.src = item.img;
+
+        var itemName = document.createElement('p');
+        itemName.setAttribute('id', 'item-name');
+        itemName.innerText = item.name;
+
+        var itemPrice = document.createElement('p');
+        itemPrice.setAttribute('id', 'item-price');
+        itemPrice.innerText = 'Price : $ ' + item.price;
+
+        itemCard.appendChild(cardTop);
+        itemCard.appendChild(img);
+        itemCard.appendChild(itemName);
+        itemCard.appendChild(itemPrice);
+
+
+        DoAnThem.appendChild(itemCard)
+
+    })
+
+    DoAnKhacData.map(item => {
+        var itemCard = document.createElement('div');
+        itemCard.setAttribute('id', 'item-card')
+
+        var cardTop = document.createElement('div');
+        cardTop.setAttribute('id', 'card-top');
+
+        var star = document.createElement('i');
+        star.setAttribute('class', 'fa fa-star');
+        star.setAttribute('id', 'rating');
+        star.innerText = ' ' + item.rating;
+
+        var heart = document.createElement('i');
+        heart.setAttribute('class', 'fa fa-heart-o add-to-cart');
+        heart.setAttribute('id', item.id)
+
+        cardTop.appendChild(star);
+        cardTop.appendChild(heart);
+
+
+        var img = document.createElement('img');
+        img.src = item.img;
+
+        var itemName = document.createElement('p');
+        itemName.setAttribute('id', 'item-name');
+        itemName.innerText = item.name;
+
+        var itemPrice = document.createElement('p');
+        itemPrice.setAttribute('id', 'item-price');
+        itemPrice.innerText = 'Price : $ ' + item.price;
+
+        itemCard.appendChild(cardTop);
+        itemCard.appendChild(img);
+        itemCard.appendChild(itemName);
+        itemCard.appendChild(itemPrice);
+
+
+        DoAnKhac.appendChild(itemCard)
+
+    })
+
+    NuocData.map(item => {
+        var itemCard = document.createElement('div');
+        itemCard.setAttribute('id', 'item-card')
+
+        var cardTop = document.createElement('div');
+        cardTop.setAttribute('id', 'card-top');
+
+        var star = document.createElement('i');
+        star.setAttribute('class', 'fa fa-star');
+        star.setAttribute('id', 'rating');
+        star.innerText = ' ' + item.rating;
+
+        var heart = document.createElement('i');
+        heart.setAttribute('class', 'fa fa-heart-o add-to-cart');
+        heart.setAttribute('id', item.id)
+
+        cardTop.appendChild(star);
+        cardTop.appendChild(heart);
+
+
+        var img = document.createElement('img');
+        img.src = item.img;
+
+        var itemName = document.createElement('p');
+        itemName.setAttribute('id', 'item-name');
+        itemName.innerText = item.name;
+
+        var itemPrice = document.createElement('p');
+        itemPrice.setAttribute('id', 'item-price');
+        itemPrice.innerText = 'Price : $ ' + item.price;
+
+        itemCard.appendChild(cardTop);
+        itemCard.appendChild(img);
+        itemCard.appendChild(itemName);
+        itemCard.appendChild(itemPrice);
+
+        Nuoc.appendChild(itemCard)
+
+    })
+}
+displayItems();
+
+
+const quancomdata = [...new Map(foodItem.map(item => [item['category'], item])).values()];
+
+
+function categoryList(quancomdata) {
+    var categoryList = document.getElementById('category-list');
+    console.log(quancomdata);
+    quancomdata.map(item => {
+        
+        var listCard = document.createElement('div');
+        listCard.setAttribute('class', 'list-card');
+
+        var listImg = document.createElement('img');
+        listImg.src = item.img;
+
+        var listName = document.createElement('a');
+        listName.setAttribute('class', 'list-name');
+        listName.innerText = item.category;
+        listName.setAttribute('href', '#' + item.category)
+
+        listCard.appendChild(listImg);
+        listCard.appendChild(listName);
+
+        var cloneListCard = listCard.cloneNode(true);
+        categoryList.appendChild(listCard);
+        
+    })
+}
+categoryList(quancomdata);
+document.querySelectorAll('.add-to-cart').forEach(item => {
+    item.addEventListener('click', addToCart)
+})
+
+var cartData = [];
+
+function addToCart() {
+
+    var itemToAdd = this.parentNode.nextSibling.nextSibling.innerText;
+    var itemObj = foodItem.find(element =>element.name == itemToAdd);
+
+    var index = cartData.indexOf(itemObj);
+    if (index === -1) {
+        document.getElementById(itemObj.id).classList.add('toggle-heart');
+        cartData = [...cartData,itemObj];
+    }
+    else if (index > -1) {
+        alert("Món ăn đã có trong giỏ hàng!!!");
+    }
+
+    document.getElementById('cart-plus').innerText = ' ' + cartData.length + ' Món Ăn ';
+    totalAmount();
+    cartItems();
+}
+
+
+function cartItems() {
+    var tableBody = document.getElementById('table-body');
+    tableBody.innerHTML = '';
+
+    cartData.map(item => {
+        var tableRow = document.createElement('tr');
+
+        var rowData1 = document.createElement('td');
+        var img = document.createElement('img');
+        img.src = item.img;
+        rowData1.appendChild(img);
+
+        var rowData2 = document.createElement('td');
+        rowData2.innerText = item.name;
+
+        var rowData3 = document.createElement('td');
+        var btn1 = document.createElement('button');
+        btn1.setAttribute('class', 'decrease-item');
+        btn1.innerText = '-';
+        var span = document.createElement('span');
+        span.innerText = item.quantity;
+        var btn2 = document.createElement('button');
+        btn2.setAttribute('class', 'increase-item');
+        btn2.innerText = '+';
+
+        rowData3.appendChild(btn1);
+        rowData3.appendChild(span);
+        rowData3.appendChild(btn2);
+
+        var rowData4 = document.createElement('td');
+        rowData4.innerText = item.price;
+
+        tableRow.appendChild(rowData1);
+        tableRow.appendChild(rowData2);
+        tableRow.appendChild(rowData3);
+        tableRow.appendChild(rowData4);
+
+        tableBody.appendChild(tableRow);
+    })
+    document.querySelectorAll('.increase-item').forEach(item => {
+        item.addEventListener('click', incrementItem)
+    })
+
+    document.querySelectorAll('.decrease-item').forEach(item => {
+        item.addEventListener('click', decrementItem)
+    })
+}
+document.getElementById('add-address').addEventListener('click', addAddress);
+
+
+function addAddress() {
+    var address = prompt('Nhập địa chỉ của bạn ', '');
+    if (address) {
+        document.getElementById('add-address').innerText = ' ' + address;
+    }
+    else {
+        document.getElementById('add-address').innerText = 'Địa Chỉ';
+        alert("Bạn chưa nhập địa chỉ!!!")
+    }
+}
+
+function incrementItem() {
+    let itemToInc = this.parentNode.previousSibling.innerText;
+    console.log(itemToInc)
+    var incObj = cartData.find(element => element.name == itemToInc);
+    incObj.quantity += 1;
+
+    currPrice = (incObj.price * incObj.quantity - incObj.price * (incObj.quantity - 1)) / (incObj.quantity - 1);
+    incObj.price = currPrice * incObj.quantity;
+    totalAmount()
+    cartItems();
+}
+var currPrice = 0;
+
+function decrementItem() {
+    let itemToInc = this.parentNode.previousSibling.innerText;
+    let decObj = cartData.find(element => element.name == itemToInc);
+    let ind = cartData.indexOf(decObj);
+    if (decObj.quantity > 1) {
+        currPrice = (decObj.price * decObj.quantity - decObj.price * (decObj.quantity - 1)) / (decObj.quantity);
+        decObj.quantity -= 1;
+        decObj.price = currPrice * decObj.quantity;
+    }
+    else {
+        document.getElementById(decObj.id).classList.remove('toggle-heart')
+        cartData.splice(ind, 1);
+        document.getElementById('cart-plus').innerText = ' ' + cartData.length + ' Items';
+        if (cartData.length < 1 && flag) {
+            document.getElementById('food-items').classList.toggle('food-items');
+            document.getElementById('category-list').classList.toggle('food-items');
+            document.getElementById('cart-page').classList.toggle('cart-toggle');
+            document.getElementById('checkout').classList.toggle('cart-toggle');
+            flag = false;
+            alert("Không còn món ăn trong giỏ hàng!!!");
+            console.log(flag)
+        }
+    }
+    totalAmount()
+    cartItems()
+}
+
+function totalAmount() {
+    var sum = 0;
+    cartData.map(item => {
+        sum += item.price;
+    })
+    document.getElementById('total-item').innerText = 'Số Lượng Món : ' + cartData.length;
+    document.getElementById('total-price').innerText = 'Tổng Tiền : $ ' + sum;
+}
+
+document.getElementById('cart-plus').addEventListener('click', cartToggle);
+
+
+var flag = false;
+function cartToggle() {
+    if (cartData.length > 0) {
+        document.getElementById('food-items').classList.toggle('food-items');
+        document.getElementById('category-list').classList.toggle('food-items');
+        document.getElementById('cart-page').classList.toggle('cart-toggle');
+        document.getElementById('checkout').classList.toggle('cart-toggle');
+        flag = true;
+        console.log(flag)
+    }
+    else {
+        alert("Hãy chọn món ăn bạn yêu thích !!!");
+    }
+}
+
+window.onresize = window.onload = function () {
+    var size = window.screen.width;
+    console.log(size)
+    if (size < 800) {
+        var cloneFoodItems = document.getElementById('food-items').cloneNode(true);
+        var cloneCartPage = document.getElementById('cart-page').cloneNode(true);
+        document.getElementById('food-items').remove();
+        document.getElementById('cart-page').remove();
+        document.getElementById('category-header').after(cloneFoodItems);
+        document.getElementById('food-items').after(cloneCartPage);
+        addEvents()
+    }
+    if (size > 800) {
+        var cloneFoodItems = document.getElementById('food-items').cloneNode(true);
+        document.getElementById('food-items').remove();
+        document.getElementById('header').after(cloneFoodItems);
+        var cloneCartPage = document.getElementById('cart-page').cloneNode(true);
+        document.getElementById('cart-page').remove();
+        document.getElementById('food-items').after(cloneCartPage);
+        addEvents()
+    }
+}
+function addEvents() {
+    document.querySelectorAll('.add-to-cart').forEach(item => {
+        item.addEventListener('click', addToCart)
+    });
+    document.querySelectorAll('.increase-item').forEach(item => {
+        item.addEventListener('click', incrementItem)
+    })
+
+    document.querySelectorAll('.decrease-item').forEach(item => {
+        item.addEventListener('click', decrementItem)
+    })
+}
+function search() {
+    var input = document.getElementById('search-input');
+    var filter = input.value.toLowerCase();
+    var items = document.querySelectorAll("#item-card");
+    if (filter === "") { 
+    alert("Hãy nhập món bạn cần tìm!!!");
+    } else {
+    const searchFood = foodItem.filter((item) =>{
+    return item.name.toLowerCase().includes(filter)
+    })
+    console.log(searchFood);
+    var food = document.getElementById('food-items');
+    food.innerHTML = ""
+     searchFood.map(item => {
+
+        var itemCard = document.createElement('div');
+        itemCard.setAttribute('id', 'item-card')
+
+        var cardTop = document.createElement('div');
+        cardTop.setAttribute('id', 'card-top');
+
+        var star = document.createElement('i');
+        star.setAttribute('class', 'fa fa-star');
+        star.setAttribute('id', 'rating');
+        star.innerText = ' ' + item.rating;
+
+        var heart = document.createElement('i');
+        heart.setAttribute('class', 'fa fa-heart-o add-to-cart');
+        heart.setAttribute('id', item.id)
+
+        cardTop.appendChild(star);
+        cardTop.appendChild(heart);
+
+
+        var img = document.createElement('img');
+        img.src = item.img;
+
+        var itemName = document.createElement('p');
+        itemName.setAttribute('id', 'item-name');
+        itemName.innerText = item.name;
+
+        var itemPrice = document.createElement('p');
+        itemPrice.setAttribute('id', 'item-price');
+        itemPrice.innerText = 'Price : $ ' + item.price;
+
+        itemCard.appendChild(cardTop);
+        itemCard.appendChild(img);
+        itemCard.appendChild(itemName);
+        itemCard.appendChild(itemPrice);
+
+        food.appendChild(itemCard);
+
+    })
+  }}
+
+document.getElementById('search-btn').addEventListener('click', () => {
+    search()
+});
+
+document.getElementById('ve-chung-toi').addEventListener('click', function() {
+    alert('Đã bán được hơn 20 năm với quán cơm tên hihi rồi chuyển sang bán cơm từ thiện.');
+  });
+  
+  document.getElementById('dich-vu').addEventListener('click', function() {
+    alert('Giao hàng tận nơi\n1 đổi 1 khi hộp cơm giao tới đổ bể\nLắng nghe góp ý từ khách hàng');
+  });
+  
+  document.getElementById('lien-he').addEventListener('click', function() {
+    alert('Mọi ý kiến thắc mắc xin liên hệ số: 0906739999');
+  });
+  
+
+  
+  
+   
+
+ 
+
+
+  
+  
---
+++ webBanHang/fooditem.js
@@ -0,0 +1,278 @@
+const foodItem= [
+    {
+    id: 1,
+    name: 'Cơm sườn',
+    category : 'Com',
+    rating : 4.3,
+    price: 20,
+    img:'food/Com/ComSuon.jpg',
+    quantity: 1
+},
+{
+    id: 2,
+    name: 'Cơm Cá Kho',
+    category : 'Com',
+    rating : 4.3,
+    price: 20,
+    img:'food/Com/ComCa.jpg',
+    quantity: 1
+},
+{
+    id: 3,
+    name: 'Cơm Sườn Trứng',
+    category : 'Com',
+    rating : 4.3,
+    price: 25,
+    img: 'food/Com/SuonTrung.jpg',
+    quantity: 1
+},
+{
+    id: 4,
+    name: 'Cơm Sườn Bì Chả',
+    category : 'Com',
+    rating : 4.3,
+    price: 35,
+    img: 'food/Com/SuonBiCha.png',
+    quantity: 1
+},
+{
+    id: 5,
+    name: 'Cơm Chả',
+    category : 'Com',
+    rating : 4.3,
+    price: 15,
+    img: 'food/Com/Comcha.png',
+    quantity: 1
+},
+{
+    id: 6,
+    name: 'Cơm Trứng Chiên',
+    category : 'Com',
+    rating : 4.3,
+    price: 15,
+    img: 'food/Com/comtrung.png',
+    quantity: 1
+},
+{
+    id: 7,
+    name: 'Cơm Sườn Chả',
+    category : 'Com',
+    rating : 4.3,
+    price: 25,
+    img: 'food/Com/suoncha.jpg',
+    quantity: 1
+},
+{
+    id: 8,
+    name: 'Cơm Ba Rọi',
+    category : 'Com',
+    rating : 4.3,
+    price: 25,
+    img: 'food/Com/combaroi.png',
+    quantity: 1
+},
+{
+    id: 9,
+    name: 'Canh Khổ Qua',
+    category : 'Canh',
+    rating : 4.3,
+    price: 10,
+    img: 'food/Canh/khoqua.jpg',
+    quantity: 1
+},
+
+{
+    id: 10,
+    name: 'Canh Chua Cá',
+    category : 'Canh',
+    rating : 4.3,
+    price: 10,
+    img: 'food/Canh/canhchua.jpg',
+    quantity: 1
+},
+
+{
+    id: 11,
+    name: 'Canh Rau Dềnh',
+    category : 'Canh',
+    rating : 4.3,
+    price: 10,
+    img: 'food/Canh/canhrauden.jpg',
+    quantity: 1
+},
+{
+    id: 12,
+    name: 'Chuối',
+    category : 'TrangMieng',
+    rating : 4.3,
+    price: 10,
+    img: 'food/TrangMieng/chuoi.jpg',
+    quantity: 1
+
+},
+{
+    id: 13,
+    name: 'Dưa Hấu',
+    category : 'TrangMieng',
+    rating : 4.3,
+    price: 10,
+    img: 'food/TrangMieng/duahau.jpg',
+    quantity: 1
+},
+{
+    id: 14,
+    name: 'Táo',
+    category : 'TrangMieng',
+    rating : 4.3,
+    price: 10,
+    img: 'food/TrangMieng/tao.jpg',
+    quantity: 1
+
+},
+{
+    id: 15,
+    name: 'Nho',
+    category : 'TrangMieng',
+    rating : 4.3,
+    price: 5,
+    img: 'food/TrangMieng/nho.jpg',
+    quantity: 1
+},
+{
+    id: 16,
+    name: 'Cơm Thêm',
+    category : 'DoAnThem',
+    rating : 4.3,
+    price: 5,
+    img: 'food/DoAnThem/comthem.jpg',
+    quantity: 1
+},
+{
+    id: 17,
+    name: 'Sườn Thêm',
+    category : 'DoAnThem',
+    rating : 4.3,
+    price: 10,
+    img: 'food/DoAnThem/suonthem.jpg',
+    quantity: 1
+},
+{
+    id: 18,
+    name: 'Trứng Thêm',
+    category : 'DoAnThem',
+    rating : 4.3,
+    price: 7,
+    img: 'food/DoAnThem/trungthem.jpg',
+    quantity: 1
+},
+{
+    id: 19,
+    name: 'Bì Thêm',
+    category : 'DoAnThem',
+    rating : 4.3,
+    price: 7,
+    img: 'food/DoAnThem/bithem.jpg',
+    quantity: 1
+},
+{
+    id: 20,
+    name: 'Chả Thêm',
+    category : 'DoAnThem',
+    rating : 4.3,
+    price: 10,
+    img: 'food/DoAnThem/chathem.jpg',
+    quantity: 1
+},
+{
+    id: 21,
+    name: 'Cá Kho Thêm',
+    category : 'DoAnThem',
+    rating : 4.3,
+    price: 10,
+    img: 'food/DoAnThem/cakho.png',
+    quantity: 1
+},
+{
+    id: 22,
+    name: 'Hủ Tíu Mì',
+    category : 'DoAnKhac',
+    rating : 4.3,
+    price: 20,
+    img: 'food/DoAnKhac/hutiumi.jpg',
+    quantity: 1
+},
+{
+    id: 23,
+    name: 'Cơm Chiên Thập Cẩm',
+    category : 'DoAnKhac',
+    rating : 4.3,
+    price: 20,
+    img: 'food/DoAnKhac/comchien.jpg',
+    quantity: 1
+},
+{
+    id: 24,
+    name: 'Hủ Tíu Mì Khô',
+    category : 'DoAnKhac',
+    rating : 4.3,
+    price: 25,
+    img: 'food/DoAnKhac/hutiumikho.jpg',
+    quantity: 1
+},
+{
+    id: 25,
+    name: 'CoCa',
+    category : 'Nuoc',
+    rating : 4.3,
+    price: 15,
+    img: 'food/Nuoc/coca.jpg',
+    quantity: 1
+},
+{
+    id: 26,
+    name: 'Sting',
+    category : 'Nuoc',
+    rating : 4.3,
+    price: 15,
+    img: 'food/Nuoc/sting.jpg',
+    quantity: 1
+},
+{
+    id: 27,
+    name: 'Nước Suối',
+    category : 'Nuoc',
+    rating : 4.3,
+    price: 5,
+    img: 'food/Nuoc/nuocsuoi.jpg',
+    quantity: 1
+},
+{
+    id: 28,
+    name: 'Milinda',
+    category : 'Nuoc',
+    rating : 4.3,
+    price: 10,
+    img: 'food/Nuoc/milinda.jpg',
+    quantity: 1
+},
+{
+    id: 29,
+    name: '7 Up',
+    category : 'Nuoc',
+    rating : 4.3,
+    price: 15,
+    img: 'food/Nuoc/7up.jpg',
+    quantity: 1
+},
+{
+    id: 30,
+    name: 'Nước Sâm',
+    category : 'Nuoc',
+    rating : 4.3,
+    price: 10,
+    img: 'food/Nuoc/nuocsam.jpg',
+    quantity: 1
+},
+]
+
+export {foodItem};
